f5857b1e6b5ec6798ee946b700b9fc77d10fdecb
Chimezie Innocent <chimezieinnocent39@gmail.com>
2025-03-04 05:06:46 +0100

    feat: Add all page docs

---
+++ config.ts
@@ -0,0 +1,11 @@
+export default {
+  theme: {
+    light: {
+      background: "#E3F2FD",
+      primary: "#90CAF9",
+      text: "#333333",
+      border: "#BBDEFB", 
+      codeBackground: "#E1F5FE",
+    },
+  },
+};
